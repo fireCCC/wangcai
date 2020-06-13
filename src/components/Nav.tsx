1c239a6470494b74bb8cfa0640f1e2d52d080860
Amber <944458473@qq.com>
2020-06-13 21:38:49 +0800

    svg and eject

---
+++ src/components/Nav.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import x from 'icons/money.svg';
+
+console.log(x);
+
+const NavWrapper = styled.nav`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  line-height: 24px;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.3333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+
+const Nav = () => {
+    return (
+        <NavWrapper>
+            <ul>
+                <li>
+                <Link to="/tags">标签</Link>
+                </li>
+                <li>
+                <Link to="/money">记账</Link>
+                </li>
+                <li>
+                <Link to="/statistics">统计</Link>
+                </li>
+            </ul>
+        </NavWrapper>
+    )
+}
+
+export default Nav;
